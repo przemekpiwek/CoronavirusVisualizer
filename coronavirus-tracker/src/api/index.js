4d472b5ae2e23f3db7a5a97709b80904c0576099
Przemek Piwek <przemekpiwek@gmail.com>
2020-06-30 17:03:11 -0600

    Graph complete

--- coronavirus-tracker/src/api/index.js
+++ coronavirus-tracker/src/api/index.js
@@ -15,5 +15,25 @@ export const fetchData = async () => {
 export const fetchDailyData = async () => {
   try {
     const { data } = await axios.get(`${url}/daily`);
+
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+
+    return modifiedData;
   } catch (err) {}
+};
+
+export const fetchCountries = async () => {
+  try {
+    const {
+      data: { countries },
+    } = await axios.get(`${url}/countries`);
+
+    return countries.map((country) => country.name);
+  } catch (err) {
+    console.log(err);
+  }
 };
